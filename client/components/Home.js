a2467d0f4d831338715fbd299384aa09ba248fb9
Ana C Sanchez <ana.sanchez6547@gmail.com>
2018-11-10 12:51:41 -0500

    first commit

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,8 +1,34 @@
-import React from 'react';
-const Home = () => {
-  return (
-    <div>Test</div>
-  )
+import React, {Component} from 'react';
+import QuoteBox from './QuoteBox.js';
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      author: '',
+    }
+  }
+
+  componentDidMount() {
+    this.newQuote();
+  }
+
+  newQuote = () => {
+    fetch('https://talaikis.com/api/quotes/random/')
+    .then(res => res.json())
+    .then(res => this.setState({ text: res.quote, author: res.author }))
+    .catch((err) => console.log(err))
+  }
+
+  render() {
+    return (
+      <div>
+        { this.state.text.length ?
+        <QuoteBox text={this.state.text} author={this.state.author} fetchQuote={this.newQuote}/> : null
+        }
+      </div>
+    )
+  }
 }
 
 export default Home;
---
+++ client/components/QuoteBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const QuoteBox = ({author, text, fetchQuote}) => (
+  <div id="quote-box">
+    <div id="text">"{text}"</div>
+    <div id="author">- {author}</div>
+    <div>
+      <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${text}%20-%20${author}`}>
+        <img width="50px" src="Twitter_Social_Icon_Circle_Color.png" />
+      </a>
+    </div>
+    <div>
+      <button id="new-quote" onClick={fetchQuote}>New Quote</button>
+    </div>
+  </div>
+)
+
+export default QuoteBox;
